a1cf4232c4ce99d99cab37af44002bd233078b89
skatzrskx55q <skatzr1997@gmail.com>
2025-08-10 14:48:56 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -14,7 +14,7 @@ import os
 def get_model():
     model_path = "fine_tuned_model"
     model_zip  = "fine_tuned_model.zip"
-    file_id    = "1RR15OMLj9vfSrVa1HN-dRU-4LbkdbRRf"  # при необходимости замените
+    file_id    = "1bZoykt0Sj2GRPvLRC_3Z7Wt8g4AGT34R"  # при необходимости замените
 
     if not os.path.exists(model_path):
         import gdown
